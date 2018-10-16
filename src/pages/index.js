78a2b5fbc8a8c2b24ef81df74cee84c5aac8531b
Loïc Nogues <nogues.loic@gmail.com>
2018-10-16 16:57:57 +0200

    Added badger

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Honey Badgers!</h1>
+    <img 
+      src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/146/badger_1f9a1.png" 
+      srcSet="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/146/badger_1f9a1.png 2x" 
+      alt="Badger on Google Android 9.0" width="120" height="120" />
   </Layout>
 )
 
