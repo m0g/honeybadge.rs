50c72db87ae223672f91596bd38dabf29dd3f8b1
Loïc Nogues <nogues.loic@gmail.com>
2018-10-16 17:00:13 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8875bdb61445d515655c.js"
+    "url": "webpack-runtime-4962723c0052ba0a00e5.js"
   },
   {
     "url": "app-0f4d6994105d1f475236.js"
@@ -36,14 +36,14 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "65b9d90b9a8793a876ed2d1751d5602f"
+    "revision": "47f84e55e01221d523275ca31dfb679a"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ee6dec5e43f53fc043fd6398878c8016"
+    "revision": "79a2c2d8d401d4732831eb1dd5487a25"
   },
   {
-    "url": "component---src-pages-index-js.6abb20ee0c0df6fb854b.css"
+    "url": "component---src-pages-index-js.c5483587ec014363ae7c.css"
   },
   {
     "url": "component---src-pages-index-js-1c2f1e0bc38d2a919f3e.js"
@@ -56,7 +56,7 @@ self.__precacheManifest = [
     "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
   },
   {
-    "url": "component---src-pages-404-js.6abb20ee0c0df6fb854b.css"
+    "url": "component---src-pages-404-js.c5483587ec014363ae7c.css"
   },
   {
     "url": "component---src-pages-404-js-7586cb1adf8350a2ee80.js"
